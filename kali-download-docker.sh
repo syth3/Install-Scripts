0579b62841ef3f9d47e3325a84575debc62f5ade
ew0k <jakebrown188@gmail.com>
2018-04-21 06:41:59 -0400

    Add line to get archive-key.asc

--- kali-download-docker.sh
+++ kali-download-docker.sh
@@ -1,3 +1,4 @@
+wget -q -O - https://archive.kali.org/archive-key.asc  | apt-key add
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 echo 'deb https://download.docker.com/linux/debian stretch stable' > /etc/apt/sources.list.d/docker.list
 apt-get update
