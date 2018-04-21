42710fa3d2bd687a6675d0271f209775eeace67a
root <root@localhost.localdomain>
2018-04-21 03:36:57 -0400

    Add apt-get update and -y on install

--- kali-download-docker.sh
+++ kali-download-docker.sh
@@ -1,7 +1,7 @@
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 echo 'deb https://download.docker.com/linux/debian stretch stable' > /etc/apt/sources.list.d/docker.list
-# apt-get update
+apt-get update
 apt-get remove docker docker-engine docker.io
-apt-get install docker-ce
+apt-get install docker-ce -y
 docker run hello-world
 systemctl start docker
