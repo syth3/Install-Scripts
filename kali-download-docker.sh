393d122a66ed009091d617ea3c14633e8453300b
ew0k <jakebrown188@gmail.com>
2018-06-11 00:07:15 -0400

    Delete kali-download-docker.sh

--- kali-download-docker.sh
+++
@@ -1,7 +0,0 @@
-wget -q -O - https://archive.kali.org/archive-key.asc  | apt-key add
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-echo 'deb https://download.docker.com/linux/debian stretch stable' > /etc/apt/sources.list.d/docker.list
-apt-get update
-apt-get remove docker docker-engine docker.io
-apt-get install docker-ce -y
-systemctl start docker
