f5be643add2a74b09bd5a4938f611be4faa03b53
ew0k <jakebrown188@gmail.com>
2018-06-10 23:53:16 -0400

    Update Docker-Download-Script.sh

--- Docker-Download-Script.sh
+++ Docker-Download-Script.sh
@@ -10,11 +10,13 @@ curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
 
 # Download docker machine (**NOTE**: Check for newest version at https://github.com/docker/machine/releases)
+#   If there is a new release, replace lines 14-16
 curl -L https://github.com/docker/machine/releases/download/v0.14.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
     chmod +x /tmp/docker-machine &&
     sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
 
 # Download docker-compose (**NOTE**: Check for newest version at https://github.com/docker/compose/releases)
+#   If there is a new release, replace lines 20-21
 curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
@@ -28,5 +30,5 @@ docker-machine --version
 docker-compose --version
 
 # Removing hello-world image
-docker rm hello-world
-docker rmi hello-world:latest
+docker rm hello-world > /dev/null
+docker rmi hello-world:latest > /dev/null
