608e29ce8de55c799e59947d217c2375ff94760f
Jake Brown <jakebrown188@gmail.com>
2019-01-30 17:29:14 -0800

    Add Intall-SaltStack initial script

---
+++ SaltStack-Install.sh
@@ -0,0 +1,12 @@
+# Install SaltStack Minion Software
+curl -L https://bootstrap.saltstack.com -o install-salt.sh
+
+# Install SaltStack Master or Minion Software
+read -p 'Install SaltStack master? (y/n): ' answer
+read -p 'IP address of SaltStack master: ' ipaddress
+if [ $answer = 'y' ]
+then
+    sudo sh install-salt.sh -M -A $ipaddress
+else
+    sudo sh install-salt.sh -A $ipaddress
+fi
