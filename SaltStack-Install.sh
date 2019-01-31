6f1e11865c7e174177e60baea64571d25c70ff09
Jake Brown <jakebrown188@gmail.com>
2019-01-30 17:49:35 -0800

    SaltStack-Install.sh: add minion ID

--- SaltStack-Install.sh
+++ SaltStack-Install.sh
@@ -1,3 +1,6 @@
+# apt-get update
+sudo apt-get update
+
 # Install curl
 sudo apt-get install curl -y
 
@@ -6,10 +9,11 @@ curl -L https://bootstrap.saltstack.com -o install-salt.sh
 
 # Install SaltStack Master or Minion Software
 read -p 'Install SaltStack master? (y/n): ' answer
-read -p 'IP address of SaltStack master: ' ipaddress
+read -p 'IP address of SaltStack master: ' ipAddress
+read -p 'Minion ID: ' minionId
 if [ $answer = 'y' ]
 then
-    sudo sh install-salt.sh -M -A $ipaddress
+    sudo sh install-salt.sh -M -A $ipaddress -i $minionId
 else
-    sudo sh install-salt.sh -A $ipaddress
+    sudo sh install-salt.sh -A $ipAddress -i $minionId
 fi
