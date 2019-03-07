bb69fa3ca1b8a4bbf5ae4e1b644ef50faa06f9a1
ew0k <jakebrown188@gmail.com>
2019-03-06 23:08:00 -0500

    Create find-files-and-copy.py

---
+++ find-files-and-copy.py
@@ -0,0 +1,29 @@
+# Have to edit this to be more generic. Currently only looks for pictures in a specific directory
+
+import os
+import shutil
+import time
+
+start = time.time()
+
+rootdir = 'D:\\Documents\\Old Computer Drive'
+destdir = 'D:\\Pictures\\Old Pictures'
+mapping_file = open('D:\\Desktop\\Find Pictures\\file-map.csv', mode='w+')
+file_types = set()
+
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        full_path = os.path.join(subdir, file)
+        full_path_split = full_path.split(".")
+        if len(full_path_split) > 1:
+            image_types = ["png", "jpg", "jpeg", "tiff", "tif", "gif", "bmp", "eps", "raw", "cr2", "nef", "orf", "sr2", "webp", "svg", "ai"]
+            if full_path_split[1].lower() in image_types:
+                name  = "Picture " + str(len(os.listdir(destdir)) + 1) + "." + full_path_split[1]
+                shutil.copy2(full_path, os.path.join(destdir, name))
+                mapping_file.write(name + "," + full_path + "\n")
+                
+mapping_file.close()
+
+end = time.time()
+diff = end - start
+print("Elapsed Time:", diff)
