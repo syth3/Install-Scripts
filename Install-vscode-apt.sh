a7cb2350a08572620bc2786e12a503ddc086e1ff
Jake Brown <syth3sec@gmail.com>
2022-04-13 13:35:56 -0400

    Update Install-vscode-apt.sh

--- Install-vscode-apt.sh
+++ Install-vscode-apt.sh
@@ -2,3 +2,5 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
 apt update && apt install code
+
+# To change the title bar to look like it does on Windows, change "Window: Title Bar Style" to custom
